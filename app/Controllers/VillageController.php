0c7f2c31d0ad6cb74e1bcbe0a4ffc7fde4755271
John <juaorok@hotmail.com>
2024-04-19 18:49:40 -0300

    Create VillageController.php

---
+++ app/Controllers/VillageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PHPvian\Controllers;
+
+class VillageController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        echo "Village Page";
+    }
+}
