f4ab762dd7ec37407e754c05eee6a5689aec5c58
John <juaorok@hotmail.com>
2024-04-19 18:48:01 -0300

    lang
    add en-us lang

---
+++ resources/views/auth/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title value="Login Page"></title>
+</head>
+<body>
+<form action="/login" method="post">
+    <div class="container">
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Enter Username" name="email">
+
+        <label for="password"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password">
+
+        <button type="submit">Login</button>
+    </div>
+</form>
+</body>
+</html>
