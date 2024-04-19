4bb051e045641ba6195f0974120c7e6a6b358895
John <juaorok@hotmail.com>
2024-04-19 18:50:04 -0300

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,8 @@
 <?php
 
+use PHPvian\Controllers\Auth\LoginController;
+use PHPvian\Controllers\Auth\LogoutController;
+use PHPvian\Controllers\Auth\SignupController;
 use PHPvian\Controllers\ErrorsController;
 use PHPvian\Controllers\IndexController;
 use PHPvian\Controllers\InstallController;
@@ -12,11 +15,14 @@ $route->get('/installer', InstallController::class, 'index');
 $route->post('/installer/process', InstallController::class, 'process');
 $route->get('/404', ErrorsController::class, 'deny');
 $route->get('/500', ErrorsController::class, 'server');
-$route->get('/login', \PHPvian\Controllers\Auth\LoginController::class, 'index');
-$route->get('/signup', \PHPvian\Controllers\Auth\SignupController::class, 'index');
-$route->get('/logout', \PHPvian\Controllers\Auth\LogoutController::class, 'logout');
-
-
+//Auth
+$route->get('/login', LoginController::class, 'index');
+$route->post('/login', LoginController::class, 'login');
+$route->get('/signup', SignupController::class, 'index');
+$route->post('/signup', SignupController::class, 'signup');
+$route->get('/logout', LogoutController::class, 'logout');
+//End Auth
+$route->get('/village', \PHPvian\Controllers\VillageController::class, 'index');
 
 
 
