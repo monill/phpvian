00986165f6008d9960e688135ed9907f32358b25
John <juaorok@hotmail.com>
2024-04-19 18:49:37 -0300

    Update IndexController.php

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -7,11 +7,11 @@ class IndexController extends Controller
     public function index()
     {
         // Checks if the file 'database.php' exists
-        if (!file_exists(dirname(__DIR__) . DIRECTORY_SEPARATOR . '../config/game.php')) {
-            header("Location: " . '/installer', true);
-            exit();
-        } else {
+//        if (!file_exists(dirname(__DIR__) . DIRECTORY_SEPARATOR . '../config/game.php')) {
+//            header("Location: " . '/installer', true);
+//            exit();
+//        } else {
             return view('index/index');
-        }
+//        }
     }
 }
