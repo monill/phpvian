87bfed4cc67d4b3f95f6ca4474d8d12dadef8f98
John <juaorok@hotmail.com>
2024-04-23 15:31:01 -0300

    remove
    removed templates

--- resources/views/install/template/menu.php
+++
@@ -1,7 +0,0 @@
-<li class="<?= empty($_GET['s']) ? "active" : "c1 f9"; ?>"><a href="#">Start</a></li>
-<li class="<?= isset($_GET['s']) && $_GET['s'] == 1 ? "active" : "c1 f9"; ?>"><a href="#">Database</a></li>
-<li class="<?= isset($_GET['s']) && $_GET['s'] == 2 ? "active" : "c1 f9"; ?>"><a href="#">Configuration</a></li>
-<li class="<?= isset($_GET['s']) && $_GET['s'] == 3 ? "active" : "c1 f9"; ?>"><a href="#">Field</a></li>
-<li class="<?= isset($_GET['s']) && $_GET['s'] == 4 ? "active" : "c1 f9"; ?>"><a href="#">Multihunter</a></li>
-<li class="<?= isset($_GET['s']) && $_GET['s'] == 5 ? "active" : "c1 f9"; ?>"><a href="#">Oasis</a></li>
-<li class="<?= isset($_GET['s']) && $_GET['s'] == 6 ? "active" : "c1 f9"; ?>"><a href="#">End</a></li>
