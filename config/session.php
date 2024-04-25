28d054d3d2b76d24e6b9fc46173d640beec1afd8
John <juaorok@hotmail.com>
2024-04-25 20:22:41 -0300

    Update session.php

--- config/session.php
+++ config/session.php
@@ -2,7 +2,6 @@
 
 return [
     //Session Configuration
-
     'session_secure' => false,
 
     'session_http_only' => true,
@@ -10,5 +9,4 @@ return [
     'session_regenerate_id' => false,
 
     'session_use_only_cookies' => true
-
 ];
