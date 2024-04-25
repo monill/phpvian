a61eee02014501c55f9709ec6239b64c6d12cd80
John <juaorok@hotmail.com>
2024-04-25 20:23:17 -0300

    Update LoginController.php

--- app/Controllers/Auth/LoginController.php
+++ app/Controllers/Auth/LoginController.php
@@ -15,6 +15,7 @@ class LoginController extends Controller
     public function __construct()
     {
         parent::__construct();
+        //$this->checkLoggedIn();
         $this->validation = new Validate();
     }
 
@@ -25,44 +26,56 @@ class LoginController extends Controller
 
     public function login()
     {
+        if (!input_exists()) {
+            redirect('/aaaaa');
+        }
+
         $email = input('email');
         $password = input('password');
 
-        $this->validLogin($email, $password);
+//        if ($this->validLogin($email, $password) > 0) {
+//            redirect('/');
+//        }
 
-        $user = new User();
-        $userExists = $user->existsByEmail($email);
+//        $user = new User();
+//        $userExists = $user->existsByEmail($email);
 
-        if (!$userExists || md5($password) !== $user->password) {
-            error_response("Invalid email or password.");
-        }
-        if (!$user->isActive()) {
-            error_response("Account not activated.");
-        }
-        if ($user->isBanned()) {
-            error_response("Account banned.");
-        }
+//        if (!$userExists || md5($password) !== $user->password) {
+//            error_response("Invalid email or password.");
+//        }
+//        if (!$user->isActive()) {
+//            error_response("Account not activated.");
+//        }
+//        if ($user->isBanned()) {
+//            error_response("Account banned.");
+//        }
 
-        Session::set("loggedIN", true);
-        Session::set("userID", (int)$user->id);
-        Session::set("username", $user->username);
-        Cookie::set("PHPvian", $user->username, 60480);
+//        if ($user) {
+//            Session::set("loggedIN", true);
+//            Session::set("userID", (int)$user->id);
+//            Session::set("username", $user->username);
+//            Cookie::set("UserN", $user->username, 60480);
+//        }
 
-        if ($this->loginFingerPrint) {
-            Session::set("loginFingerPrint", $this->generateLoginString());
-        }
+//        if ($this->loginFingerPrint) {
+//            Session::set("loginFingerPrint", $this->generateLoginString());
+//        }
 
-        redirect("/village");
+//        redirect("/village");
     }
 
     public function validLogin($email, $password)
     {
+        $error = 0;
         if ($this->validation->isEmpty($email) || $this->validation->isEmpty($password)) {
+            $error += 1;
             error_response("Please enter both email and password.");
         }
         if (!$this->validation->emailExist($email)) {
+            $error += 1;
             error_response("Email not exists...");
         }
+        return $error;
     }
 
 }
