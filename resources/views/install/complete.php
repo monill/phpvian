a31ea7c22483281a4206abce1b85ef2b156f81ac
John <juaorok@hotmail.com>
2024-05-05 22:46:17 -0300

    Create complete.php

---
+++ resources/views/install/complete.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Travian Installer</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://license.viserlab.com/external/install.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <style>
+        #hide {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+<div class="installation-section padding-bottom padding-top">
+    <div class="container">
+        <div class="installation-wrapper">
+            <div class="install-content-area">
+                <div class="installation-wrapper pt-md-5">
+                    <ul class="installation-menu">
+                        <li class="steps done">
+                            <div class="thumb">
+                                <i class="fas fa-server"></i>
+                            </div>
+                            <h5 class="content">Server<br>Requirements</h5>
+                        </li>
+                        <li class="steps done">
+                            <div class="thumb">
+                                <i class="fas fa-file-signature"></i>
+                            </div>
+                            <h5 class="content">File<br>Permissions</h5>
+                        </li>
+                        <li class="steps done">
+                            <div class="thumb">
+                                <i class="fas fa-database"></i>
+                            </div>
+                            <h5 class="content">Installation<br>Information</h5>
+                        </li>
+                        <li class="steps done">
+                            <div class="thumb">
+                                <i class="fas fa-check-circle"></i>
+                            </div>
+                            <h5 class="content">Complete<br>Installation</h5>
+                        </li>
+                    </ul>
+                </div>
+                <div class="installation-wrapper">
+                    <div class="install-content-area">
+                        <div class="install-item">
+                            <h3 class="bg-warning title text-center">Installation Complete!</h3>
+                            <div class="box-item">
+                                <div class="item">
+                                    <p class="info">
+                                        Congratulations, you survived the installation! Now, get ready to dive into the wonderful world of your new website.
+                                    </p>
+                                    <p class="info">
+                                        Here are the next steps you can take to get started:
+                                    </p>
+                                    <p class="info">
+                                        System Access: Visit the URL address you configured during installation and prepare to marvel at the masterpiece you just created.
+                                    </p>
+                                    <p class="info">
+                                        Explore Features: Let your imagination run wild and try out all the system’s features. We want to see what you can do!
+                                    </p>
+                                    <p class="info">
+                                        And, if by any chance you have any questions, concerns or crazy ideas, feel free to... well, go it alone! We are confident that you are more than capable of resolving any issue that may arise.
+                                    </p>
+                                    <p class="info">
+                                        <b>Enjoy your new website and may creativity always be by your side!</b>
+                                    </p>
+                                    <p class="info">Yours sincerely, <br>PHPvian Team</p>
+                                </div>
+                                <div class="item text-right">
+                                    <a href="/" class="theme-button choto">Continue</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
