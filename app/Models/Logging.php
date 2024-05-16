08a7b4e9f4a1c328d41d763f7059b8ffc2f55f1e
John <juaorok@hotmail.com>
2024-05-16 16:14:00 -0300

    Create Logging.php

---
+++ app/Models/Logging.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace PHPvian\Models;
+
+use PHPvian\Libs\Connection;
+
+class Logging
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = new Connection();
+    }
+
+    public function addIllegal($uid, $ref, $type)
+    {
+        global $database;
+        if (LOG_ILLEGAL) {
+            $log = "Attempted to ";
+            switch ($type) {
+                case 1:
+                    $log .= "access village $ref";
+                    break;
+            }
+            $q = "Insert into illegal_log values (0,$uid,'$log')";
+            $database->query($q);
+        }
+    }
+
+    public function addLoginLog($userID)
+    {
+        if (setting('log_login')) {
+            $this->conn->insert('login_log', [
+                'id' => 0,
+                'user_id' => $userID,
+                'ip' => get_ip()
+            ]);
+        }
+    }
+
+    public function addBuildLog($wid, $building, $level, $type)
+    {
+        global $database;
+        if (LOG_BUILD) {
+            if ($type) {
+                $log = "Start Construction of ";
+            } else {
+                $log = "Start Upgrade of ";
+            }
+            $log .= $building . " at level " . $level;
+            $q = "Insert into build_log values (0,$wid,'$log')";
+            $database->query($q);
+        }
+    }
+
+    public function addTechLog($wid, $tech, $level)
+    {
+        global $database;
+        if (LOG_TECH) {
+            $log = "Upgrading of tech " . $tech . " to level " . $level;
+            $q = "Insert into tech_log values (0,$wid,'$log')";
+            $database->query($q);
+        }
+    }
+
+    public function goldFinLog($wid)
+    {
+        global $database;
+        if (LOG_GOLD_FIN) {
+            $log = "Finish construction and research with gold";
+            $q = "Insert into gold_fin_log values (0,$wid,'$log')";
+            $database->query($q);
+        }
+    }
+
+    public function addAdminLog()
+    {
+        //global $database;
+    }
+
+    public function addMarketLog($wid, $type, $data)
+    {
+        global $database;
+        if (LOG_MARKET) {
+            if ($type == 1) {
+                $log = "Sent " . $data[0] . "," . $data[1] . "," . $data[2] . "," . $data[3] . " to village " . $data[4];
+            } else if ($type == 2) {
+                $log = "Traded resource between " . $wid . " and " . $data[0] . " market ref is " . $data[1];
+            }
+            $q = "Insert into market_log values (0,$wid,'$log')";
+            $database->query($q);
+        }
+    }
+
+    public function VillageDestroyCatalog($wid)
+    {
+        global $database;
+        if (LOG_GOLD_FIN) {
+            $log = "Village destroyed";
+            $q = "Insert into destroy_log values (0,$wid,'$log')";
+            $database->query($q);
+        }
+    }
+
+    public function addWarLog()
+    {
+        //global $database;
+    }
+
+    public function clearLogs()
+    {
+        //global $database;
+    }
+
+    public function debug($time, $uid, $debug_info)
+    {
+        global $database;
+
+        //$debugFile = "/tmp/debug";
+        //$fh = fopen($debugFile, 'a') or die('No debug file');
+        //fwrite($fh,"\n".date("Y-m-d H:i:s")." : ".$time.",".$uid.",".$debug_info."\n");
+        //fclose($fh);
+
+        $q = "INSERT INTO debug_log (time,uid,debug_info) VALUES ($time,$uid,'$debug_info')";
+        $database->query($q);
+    }
+}
