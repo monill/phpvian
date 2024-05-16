c16c945c84cbfc1b6abf2305544f9f69c64cc9a4
John <juaorok@hotmail.com>
2024-05-16 16:13:19 -0300

    Create welcome.php

---
+++ resources/views/templates/messages/welcome.php
@@ -0,0 +1,15 @@
+Hello %USER%,
+
+Travian is a game of domination through strategy and diplomacy. At the core, it is about standing up against other players. Seek allies to help you achieve your goals; but even the strongest players require assistance.
+
+The predominant task is always to survive and grow. In order to start on a sound footing, simply do the tasks I have given you. Protect your resources and villages from greedy neighbors. In case you are struggling, you can find some additional help here. Help, I am a farmer!
+
+At the start, you can often avoid enemy attacks, but you should defend yourself against conquering attempts. Without troops, you wouldn't be able to help yourself or your allies. More information here: Troops.
+
+Once the dust of the first battles has settled, the strongest alliances will start fighting for control of ever larger territories. They will destroy threats and competitors and form new alliances.
+
+An important starting hint - artifacts are tied to ancient cities. You can't steal them and cart them off over the hills. You need to conquer the city to gain the effects of the artifact. In fact, when you have one, you have to choose how you will benefit from it: the artifact bonus or victory points. Please check the forums for more information about this.
+
+Adjust your personal goals to the time you can spend playing - the more ambitious your aims, the more time will be required to build, negotiate and fight.
+
+But enough of the talking! Get together with old and new friends, fight your enemies - and have fun together!
