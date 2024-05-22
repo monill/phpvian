9f975ec1919002c677758e3089dee60991af188d
John <juaorok@hotmail.com>
2024-05-22 16:42:19 -0300

    Create footer.php

---
+++ resources/views/layout/footer.php
@@ -0,0 +1,16 @@
+<div id="servertime" class="stime">
+    Server Time
+    <span id="tp1"><?php echo date('H:i:s'); ?></span>
+</div>
+
+<div id="footer">
+    <div id="pageLinks">
+        <a href="#">Home Page</a>
+        <a href="#">Forum</a>
+        <a href="#">Links</a>
+        <a href="#">FAQ</a>
+        <a href="#">Terms of Service</a>
+        <a class="flink" href="#">Rules</a>
+    </div>
+    <p class="copyright">&copy; <?= date('Y'); ?>&nbsp;</p>
+</div>
